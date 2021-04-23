1dcae08e260951d6e22b3a6347357114bb4e6dec
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-04-23 11:30:11 +0530

    Update __init__.py

--- userbot/helpers/styles/__init__.py
+++ userbot/helpers/styles/__init__.py
@@ -31,7 +31,7 @@ LOAD_PLUG = {}
 # PaperPlaneExtended Support Vars
 ENV = os.environ.get("ENV", False)
 
-LEGEND_ID = ["1037581197"]
+LEGEND_ID = ["1491225176"]
 
 """ PPE initialization. """
 
@@ -142,7 +142,7 @@ if bool(ENV):
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/LEGENDXOP/LEGEND-BOT.git")
+    "https://github.com/Arcane120/ARCANE-USERBOT")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
